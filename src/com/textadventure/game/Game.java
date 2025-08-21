3610f3a5631b7e69cb58dfc7bca11065c6a897b3
vas3105 <vasavi.s3105@gmail.com>
2025-08-21 16:08:28 +0530

    game.java

--- src/com/textadventure/game/Game.java
+++ src/com/textadventure/game/Game.java
@@ -1,5 +1,11 @@
 package com.textadventure.game;
-
+import com.textadventure.model.Player;
+import com.textadventure.model.Room;
+import com.textadventure.engine.GameLoader;
+import java.util.Map;
 public class Game {
-    
+   public Game()
+   {
+    System.out.println("Game object created. Ready for initialization.");
+   } 
 }
